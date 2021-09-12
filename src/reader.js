523df127b53c2c8237c99b9f219a5b98f21a84e5
milankharal <milankharal@adt.com>
2021-09-12 14:10:59 -0500

    user input and exit mechanism

---
+++ src/reader.js
@@ -0,0 +1,62 @@
+const readline = require('readline');
+
+const { stdin, stdout } = process;
+
+const reader = readline.createInterface({
+    input: stdin,
+    output: stdout,
+    terminal: false,
+});
+
+readline.emitKeypressEvents(process.stdin);
+
+// raw mode to lisen to each characters separately
+stdin.setRawMode(true);
+
+reader.on("line", (userInput) => {
+    // just for conveniene, if you type clear and enter it will clear the current stack
+    if (userInput === "clear") {
+        // TODO: clear the stack later once we have that
+        console.log("\n stack is cleared");
+        return;
+    }
+    // for convenience, exit will terminate the process
+    if (userInput === "exit") {
+        process.exit();
+    }
+
+    // when there is user input that needs to be processed
+    if (userInput) {
+        // TODO: validate and process the input
+    } else {
+        // ignore 
+        console.log("\n invalid input")
+    }
+
+})
+
+stdin.on("keypress", (str, key) => {
+    if (key.ctrl) {
+        if (key.name.toLowerCase() === "c") {
+            //keyboard interrupt
+            console.log("Keyboard interrupt, exiting ...");
+            process.exit();
+        } else if (
+            key.name.toLowerCase === "d" ||
+            key.name.toLowerCase === "z"
+        ) {
+            //end of file
+            console.log("EOF, exiting ...");
+            process.exit();
+        }
+    } else if (
+        key &&
+        key.name && 
+        key.name.toLowerCase() === "q"    
+    ) {
+        // q keyword
+        console.log("quitting ...")
+        process.exit();
+    }
+    stdout.write(str);
+})
